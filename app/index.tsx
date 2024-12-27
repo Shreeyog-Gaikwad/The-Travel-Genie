b7e4502d55619b925d634af3374e743459f87739
Shreeyog Gaikwad <shreeyoggaikwad@gmail.com>
2024-12-27 11:18:40 +0530

    Fonts Added / Files Cleaned

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style = {{
+         fontSize : 30,
+         fontFamily : 'outfit-bold'
+      }}>Hello Shreeyog!!! </Text>
+    </View>
+  );
+}
